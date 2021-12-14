67d96056901d49cad47a0d5c12f17cbfa29cfe51
SigneKirkBrødbæk#5330 <signebroedbaek@gmail.com>
2021-12-14 15:59:17 +0100

    randomize data added

--- DanishBERTBotXO.py
+++ DanishBERTBotXO.py
@@ -3,7 +3,7 @@ from utility_fcs import idx_occ_pron, remove_sq_br, load_texts, get_pred_res
 from predict_mask import predict_masked
 from group_pronouns import group_pronouns
 from model_evaluation import evaluate_model
-import torch, os, spacy
+import torch, os, spacy, random 
 
 model = "Maltehb/danish-bert-botxo"
 nlp =  pipeline(task = "fill-mask", model = model) 
@@ -11,13 +11,19 @@ tokenizer = spacy.load("da_core_news_lg")
 
 #load data set
 anti_lines, pro_lines = [], []
-path = os.path.join("NLP","Detecting-Bias-in--LMs","data")
+path = os.path.join("nlp","Detecting-Bias-in--LMs","data")
 anti_lines = load_texts(path,"anti", "both")
 pro_lines = load_texts(path,"pro", "both")
 
 # flatten lists
 anti_lines = [sentence for sublist in anti_lines for sentence in sublist]
 pro_lines = [sentence for sublist in pro_lines for sentence in sublist]
+
+# randomize data 
+random.shuffle(anti_lines)
+random.shuffle(pro_lines)
+print(anti_lines)
+print(pro_lines)
 
 #mask and predict pronoun 
 anti_labels, anti_preds = predict_masked(lines = anti_lines, nlp = nlp, tokenizer = tokenizer)
