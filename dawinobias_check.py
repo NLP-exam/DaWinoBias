6ad0279edbd156ad03ea3f0e6bc2b3342d524812
SigneKirkBrødbæk#5330 <signebroedbaek@gmail.com>
2021-12-14 14:41:03 +0100

    update dawinobias_check

--- dawinobias_check.py
+++ dawinobias_check.py
@@ -1,6 +1,9 @@
-import os
-from utility_fcs import load_texts, load_occs
+import os, spacy
+from utility_fcs import load_texts, load_occs, remove_sq_br
 from collections import Counter
+
+#load model used for tokenization
+nlp = spacy.load("da_core_news_lg") 
 
 # load data - we just use anti for this, since pro and anti are identical (except for pronouns)
 path = os.path.join("nlp","Detecting-Bias-in--LMs","data")
@@ -10,8 +13,8 @@ anti_lines = load_texts(path,"anti", "both")
 anti_lines = [sentence for sublist in anti_lines for sentence in sublist]
 
 # make data one string
-anti_lines = ' '.join(anti_lines)
-anti_lines = anti_lines.lower()
+anti_lines_str = ' '.join(anti_lines)
+anti_lines_str = anti_lines_str.lower()
 
 # load occupations
 all_occ, occ_no_poss = load_occs(female=True,male=True)
@@ -19,25 +22,39 @@ _, f_occ_no_poss = load_occs(female=True)
 _, m_occ_no_poss = load_occs(male=True)
 
 # count of each occupation is present in data 
-occ_dic = {occ: anti_lines.count(occ) for occ in occ_no_poss}
+occ_dic = {occ: anti_lines_str.count(occ) for occ in occ_no_poss}
 occ_dic['assistenten'] = occ_dic['assistenten'] - occ_dic['kontorassistenten'] - occ_dic['rengøringsassistenten'] 
 
-# get count/percentage of how many female vs. male stereotypical occupations
-stereo_f_dic = {occ: anti_lines.count(occ) for occ in f_occ_no_poss}
-stereo_m_dic = {occ: anti_lines.count(occ) for occ in m_occ_no_poss}
+# get percentage of how many female vs. male stereotypical occupations
+stereo_f_dic = {occ: anti_lines_str.count(occ) for occ in f_occ_no_poss}
+stereo_m_dic = {occ: anti_lines_str.count(occ) for occ in m_occ_no_poss}
+f_sum = sum(stereo_f_dic.values())
+m_sum = sum(stereo_m_dic.values())
+total = f_sum + m_sum
+print("% female and male stereotypical occupations in DaWinoBias")
+print(round(f_sum/total,2),round(m_sum/total,2))
+
+# Position check
+pos_0=''
+for line in anti_lines:
+    # make line nlp object
+    line = nlp(line)
+
+    # tokenize and lowercase
+    tokens = []
+    for token in line:
+        tokens.append(token.text.lower())
+
+    #remove [ and ]
+    tokens = remove_sq_br(tokens)[0]
+
+    # get 
+    pos_0 += tokens[0] + ' '
+
+print("% stereotypical female and male occupations that are the first element in the sentences")
+stereo_f_dic = {occ: pos_0.count(occ) for occ in f_occ_no_poss}
+stereo_m_dic = {occ: pos_0.count(occ) for occ in m_occ_no_poss}
 f_sum = sum(stereo_f_dic.values())
 m_sum = sum(stereo_m_dic.values())
 total = f_sum + m_sum
 print(round(f_sum/total,2),round(m_sum/total,2))
-
-# check position? 
-# tokenize sentences 
-line = nlp(line)
-
-# tokenize and lowercase
-tokens = []
-for token in line:
-    tokens.append(token.text.lower())
-
-tokens = remove_sq_br(tokens)[0]
-occ_idx [tokens.index(token) for token in tokens if token in all_occ]
